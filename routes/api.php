e2b15dfc25e3f793e7d4f93867abc25d239d7b53
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-02-21 01:25:59 +0200

    Removed sensitive data from commit history

--- routes/api.php
+++ routes/api.php
@@ -122,8 +122,8 @@ Route::group(['middleware' => ['json.response']], function () {
     Route::post('/verify-otp-order', [FinanceController::class, 'verifyOtp']);
     // Route::post('/cash-Order', [FinanceController::class, 'sendOtp']);
     Route::post('/cash-Order', [FinanceController::class, 'validationcash']);
-    Route::post('/financecar-Order', [FinanceController::class, 'validationfinance']);
-
+    Route::post('/financecar-Order', function () {
+dd('dsds');    });
 
 
     Route::get('/cities', [citiyController::class, 'index']);
