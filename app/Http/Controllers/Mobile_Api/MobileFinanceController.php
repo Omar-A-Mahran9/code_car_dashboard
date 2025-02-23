a2d5c3052530151926dd99c04a94cad8afc952ec
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-02-23 13:52:29 +0200

    Removed sensitive data from commit history

--- app/Http/Controllers/Mobile_Api/MobileFinanceController.php
+++ app/Http/Controllers/Mobile_Api/MobileFinanceController.php
@@ -140,20 +140,19 @@ class MobileFinanceController extends Controller
                 $request['car'] = $car;
                 $data           = $this->calculateInstallmentscar($request);
               }
-              return $this->success(data: $data);
+           $carResource = CarResourse::make($car)->resolve();
+
+              return $this->success(data: [$data,'car details'=>[
+                // "brand" => $carResource['brand']['title'],
+                // "model" => $carResource['model']['title'],
+                // "category" => $carResource['categories']['title']??null,
+                // "year" => $carResource['year'],
+                // "gear_shifter"=>$carResource['gear_shifter'],
+                "color"=>$carResource['color'],
+              ]]);
               break;
 
-                    //   $carResource = CarResourse::make($car)->resolve();
 
-
-                    //   return [
-                    //     "brand" => $carResource['brand']['title'],
-                    //     "model" => $carResource['model']['title'],
-                    //     "category" => $carResource['categories']['title']??null,
-                    //     "year" => $carResource['year'],
-                    //     "gear_shifter"=>$carResource['gear_shifter'],
-                    //     "color"=>$carResource['color'],
-                    //      ];
               break;
         case 2:
             $request->validate([
