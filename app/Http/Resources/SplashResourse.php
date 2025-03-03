28442ad4438960a1c7e706c360a319a5c3f9eee2
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-03-03 04:01:36 +0200

    Removed .DS_Store and updated .gitignore

--- app/Http/Resources/SplashResourse.php
+++ app/Http/Resources/SplashResourse.php
@@ -17,7 +17,11 @@ class SplashResourse extends JsonResource
         return [
             'id' => $this->id,
             'title'=> $this->name,
+            'title_ar'=> $this->name_ar,
+            'title_en'=> $this->name_en,
             'description'=> $this->description,
+            'description_ar'=> $this->description_ar,
+            'description_en'=> $this->description_en,
 
             'image'=>getImagePathFromDirectory($this->image,'Splashes'),
 
