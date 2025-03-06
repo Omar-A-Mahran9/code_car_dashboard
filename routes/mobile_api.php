44e59d518721e33ef86c5655fbe1eab089b92ddb
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-03-06 13:42:14 +0200

    firat commit

--- routes/mobile_api.php
+++ routes/mobile_api.php
@@ -60,12 +60,13 @@ Route::group(['middleware' => ['json.response']], function () {
         Route::post('/add-favorite-withauth', 'Api\FavoriteController@store');
         Route::get('/requests_auth', [RequestController::class,'index'])->name('get-requests');
 
-        Route::post('/car-notfound-Order', [Mobile_ApiFinanceController::class, 'notfoundcar']);
 
         Route::post('/finance-Order', [Mobile_ApiFinanceController::class, 'financeOrder'])->name('finance.order');
         Route::post('/finance-dash', [Mobile_ApiFinanceController::class, 'financeOrderDashboard'])->name('finance.orde');
         Route::post('/favorite-auth', 'Api\UserController@favorite');
     });
+    Route::post('/car-notfound-Order', [Mobile_ApiFinanceController::class, 'notfoundcar']);
+
     Route::post('/favorite-withoutauth', 'Api\UserController@favorite');
     Route::post('/add-favorite-withoutauth', 'Api\FavoriteController@store');
     Route::post('/add-array-favorite', 'Api\FavoriteController@storeFavArray');
