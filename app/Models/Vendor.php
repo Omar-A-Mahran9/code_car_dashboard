d2767087dda63159556b3af46df3d08a08e50e35
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-04-24 12:42:19 +0200

    firat commit

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -5,8 +5,6 @@ namespace App\Models;
 use App\Enums\VendorStatus;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
