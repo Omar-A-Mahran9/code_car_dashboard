c1cf55db6ff5cf34bb500c9d9f12e0986d28655a
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-05-15 16:35:42 +0300

    firat commit

--- resources/views/dashboard/orders/show.blade.php
+++ resources/views/dashboard/orders/show.blade.php
@@ -1072,7 +1072,7 @@
 
                             </div>
 
-                            @if ($order['orderDetailsCar']['type'] == 'individual')
+                    @if (($order['orderDetailsCar']['type'] ?? null) === 'individual')
                                 <div class="ps-4">
                                     <a href="https://wa.me/{{ $order['phone'] }}?text={{ urlencode(__('السلام عليكم. موقع كود كار للسيارات يرحب بكم ويسعدنا التواصل معك بخصوص طلبك رقم ' . $order['id'] . ' لسيارة: ' . $order['car_name'] . '')) }}"
                                         target="_blank" title="Chat on WhatsApp" class="whatsapp-icon">
